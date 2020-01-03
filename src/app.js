f5be17ae0a50c66c14b78e513de8937ff2c75b6b
ericmustin <mustin.eric@gmail.com>
2020-01-03 13:45:18 +0100

    indentation fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 async function getApp() {
-	const express = require('express')
-	const app = express()
-	return app
+  const express = require('express')
+  const app = express()
+  return app
 }
 
 export default getApp
--- src/tracer.js
+++ src/tracer.js
@@ -2,23 +2,23 @@
 
 import tracer from 'dd-trace';
 import axios from 'axios';
-	
+
 async function getTracer() {
-	let response
-	try {
-		response = await axios('http://169.254.169.254/latest/meta-data/local-ipv4', {timeout: 2000})	
-	} catch(e) {
-		console.warn('error fetching agent hostname')
-	}
-	
-	const host = response && response.metadata || 'localhost'
+  let response
+  try {
+    response = await axios('http://169.254.169.254/latest/meta-data/local-ipv4', {timeout: 2000})	
+  } catch(e) {
+    console.warn('error fetching agent hostname')
+  }
 
-	if(!(response && response.metadata)) {
-		console.warn('response metadata missing, defaulting to localhost')	
-	}
+  const host = response && response.metadata || 'localhost'
 
-	tracer.init({ debug: true, hostname: host });
-	return tracer
+  if(!(response && response.metadata)) {
+    console.warn('response metadata missing, defaulting to localhost')	
+  }
+
+  tracer.init({ debug: true, hostname: host });
+  return tracer
 }
 
 export default getTracer
